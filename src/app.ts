86877e9ef34577e498ce78e3f6f770228d52b9d8
Sanchita Devi <thesanchitadevi@gmail.com>
2025-02-22 20:29:22 +0600

    changes

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 // Code for the app
-import express, { Application, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import router from './app/routes';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
 
-const app: Application = express();
+const app = express();
 
 //parse application/json
+app.use(
+  cors({
+    origin: 'https://bike-store-client-alpha.vercel.app',
+    credentials: true,
+  }),
+);
 app.use(express.json());
-app.use(cors({ origin: 'https://bike-store-client-alpha.vercel.app', credentials: true }));
 
 // application routes
 app.use('/api', router);
