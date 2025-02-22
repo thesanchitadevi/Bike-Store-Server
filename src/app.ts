8fd1133d41b65fe2910060f73d4e074b0215177d
Sanchita Devi <thesanchitadevi@gmail.com>
2025-02-22 20:22:00 +0600

    changes

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 
 //parse application/json
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://bike-store-client-alpha.vercel.app', credentials: true }));
 
 // application routes
 app.use('/api', router);
